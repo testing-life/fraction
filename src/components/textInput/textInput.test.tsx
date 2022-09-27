490b793352d84149b6189b61e685e9ac3ae9083b
Mariusz Hussariush <mariusz@curvelabs.eu>
2022-09-27 11:47:14 +0200

    test

--- src/components/textInput/textInput.test.tsx
+++ src/components/textInput/textInput.test.tsx
@@ -1,7 +1,6 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import TextInput from './textInput';
-// import userEvent from '@testing-library/user-event';
 
 describe('TextInput', () => {
   it('renders', () => {
@@ -16,12 +15,12 @@ describe('TextInput', () => {
     expect(inputField).toHaveAttribute('placeholder', "I'm a holder");
   });
 
-  it('has a change event handler', async () => {
-    // const user = userEvent.setup();
-    // const changeHandlerMock = jest.fn();
-    // render(<TextInput label="I'm a label" placeholder="I'm a holder" id="input-id" onChange={changeHandlerMock} />);
-    // const inputField = screen.getByRole('textbox');
-    // await user.type(inputField, 'Test');
-    // expect(changeHandlerMock).toHaveBeenCalled();
+  it('calls a change event handler', async () => {
+    const user = userEvent;
+    const changeHandlerMock = jest.fn();
+    render(<TextInput label="I'm a label" placeholder="I'm a holder" id="input-id" onChange={changeHandlerMock} />);
+    const inputField = screen.getByRole('textbox');
+    await user.type(inputField, 'Test');
+    expect(changeHandlerMock).toHaveBeenCalled();
   });
 });
